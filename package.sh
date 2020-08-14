1e450dc0aa7a9a644b2d01b15e7956ded2f7183c
Martin Skuta <mskuta@posteo.net>
2020-08-14 22:24:53 +0200

    Expand list of possible dependencies

--- package.sh
+++ package.sh
@@ -22,7 +22,7 @@ cat <<END >"$PKGNAME/DEBIAN/control"
 Package: $PRJNAME
 Version: $VERSION
 Architecture: all
-Depends: php-cli | php5-cli | php7-cli, mariadb-client | mariadb-client-5.5 | mariadb-client-10.0 | mariadb-client-10.1 | mariadb-client-10.3 | mariadb-client-10.4 | mysql-client | mysql-client-5.5 | mysql-client-5.7
+Depends: php-cli | php5-cli | php7-cli, mariadb-client | mariadb-client-5.5 | mariadb-client-10.0 | mariadb-client-10.1 | mariadb-client-10.3 | mariadb-client-10.4 | mysql-client | mysql-client-5.5 | mysql-client-5.7 | mysql-client-8.0
 Description: Change the URL of a WordPress site
 Homepage: https://github.com/mskuta/$PRJNAME
 Maintainer: Martin Skuta (https://github.com/mskuta)
